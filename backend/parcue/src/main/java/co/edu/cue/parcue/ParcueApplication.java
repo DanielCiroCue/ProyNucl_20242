f315fa60722f6282737c585c9aec447082fedfd0
Daniel Ciro <deciro@uniquindio.edu.co>
2024-10-19 12:00:39 -0500

    Primer Commit

---
+++ backend/parcue/src/main/java/co/edu/cue/parcue/ParcueApplication.java
@@ -0,0 +1,13 @@
+package co.edu.cue.parcue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParcueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParcueApplication.class, args);
+	}
+
+}
